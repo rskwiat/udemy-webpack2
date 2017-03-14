fa2ea244ae9b7680fb3642bef67bed445b5eafbe
Robert Skwiat <rskwiat@gmail.com>
2017-03-14 11:32:15 -0400

    init files

--- WebpackProject/webpack.config.js
+++ WebpackProject/webpack.config.js
@@ -1,10 +1,54 @@
 var webpack = require('webpack');
 var path = require('path');
+var htmlWebpack = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  "faker",
+  "lodash",
+  "react",
+  "react-dom",
+  "react-input-range",
+  "react-redux",
+  "react-router",
+  "redux",
+  "redux-form",
+  "redux-thunk"
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: '[name].[chunkhash].js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/,
+        exclude: /\node_modules/
+      },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor', 'manifest']
+    }),
+    new htmlWebpack({
+      template: './src/index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 };
